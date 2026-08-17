0d8c3dc746b8db089a03cbd66e80e66bb0542c85
Abhay Dandge <abhay.d.patil96@gmail.com>
2026-08-17 12:59:01 +0530

    Commit apps/web/tailwind.config.js to branch byob

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['Fira Code', 'JetBrains Mono', 'monospace'],
+      },
+      colors: {
+        brand: {
+          50: '#f0fdfa',
+          100: '#ccfbf1',
+          500: '#14b8a6',
+          600: '#0d9488',
+          900: '#134e4a',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
